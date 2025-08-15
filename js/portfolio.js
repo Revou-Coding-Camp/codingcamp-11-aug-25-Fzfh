1ba1250cdff07aa1d41235fbbd9dbc35e26abf2f
Fzfh <fazrihafidz120@gmail.com>
2025-08-15 19:05:29 +0700

    Saya sudah Menyelesaikan nya kak

---
+++ js/portfolio.js
@@ -0,0 +1,24 @@
+function tampilkanPesan(event) {
+  event.preventDefault();
+
+  const nama = document.getElementById('name').value;
+  const ttl = document.getElementById('ttl').value;
+  const gender = document.querySelector('input[name="gender"]:checked')?.value || "belum di pilih jenis kelamin";
+  const pesan = document.getElementById('message').value;
+
+  const waktu = new Date();
+
+  const output = document.getElementById('output');
+  output.innerHTML = `
+    <div class="contact-item">
+      <div class="contact-item-detail">
+        <p><b>Current Time:</b> ${waktu}</p>
+        <br>
+        <p><b>Nama:</b> ${nama}</p>
+        <p><b>Tanggal Lahir:</b> ${ttl}</p>
+        <p><b>Jenis Kelamin:</b> ${gender}</p>
+        <p><b>Pesan:</b> ${pesan}</p>
+      </div>
+    </div>
+  `;
+}
